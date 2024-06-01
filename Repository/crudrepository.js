940b30db3f1feffedcd29ea057d852e02baf2ea2
Shishir <shishir.lakshmivenkatashishir@realpage.com>
2024-06-01 18:16:41 +0530

    crud commit

---
+++ Repository/crudrepository.js
@@ -0,0 +1,59 @@
+class CrudRepository {
+    constructor(model) {
+      this.model = model;
+    }
+  
+    async create(data) {
+      try {
+        const result = await this.model.create(data);
+        return result;
+      } catch (error) {
+        console.log("Something went wrong ");
+        console.log(error);
+      }
+    }
+  
+    async delete(id) {
+      try {
+        const result = await this.model.findByIdAndDelete(id);
+        return result;
+      } catch (error) {
+        console.log("Something went wrong");
+        console.log(error);
+      }
+    }
+  
+    async getById(id) {
+      try {
+        const result = await this.model.findById(id);
+        return result;
+      } catch (error) {
+        console.log("Something went wrong");
+        console.log(error);
+      }
+    }
+  
+    async getAll() {
+      try {
+        const result = await this.model.find({});
+        return result;
+      } catch (error) {
+        console.log("Something went wrong");
+        console.log(error);
+      }
+    }
+  
+    async update(id, data) {
+      try {
+        const result = await this.model.findByIdAndUpdate(id, data, {
+          new: true,
+        });
+        return result;
+      } catch (error) {
+        console.log("Something went wrong");
+        console.log(error);
+      }
+    }
+  }
+  
+  export default CrudRepository;
