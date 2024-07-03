4689aeba4e1015b519c31ca48552872fca0fd5dd
Sai Tejas <kamma.saitejas@realpage.com>
2024-07-03 22:12:03 +0530

    a

--- controllers/CriticsController.js
+++ controllers/CriticsController.js
@@ -1,10 +1,23 @@
 const Critics=require('../models/critics');
 const getCritics=async (req,res)=>{
     try {
-        const response=await Critics.find({});
+        const response=await Critics.find({product_name:req.params.pid});
         res.json(response);
     } catch (error) {
         console.log(error);
     }
 }
-module.exports=getCritics;
+
+const getOnlyNameCritics = async(req,res)=>{
+    try
+    {
+        const response = await Critics.find({});
+        const data = response.map(o=>o.product_name);
+        res.json(data);
+    }
+    catch(e)
+    {
+        console.log(e);
+    }
+}
+module.exports={getCritics,getOnlyNameCritics};
